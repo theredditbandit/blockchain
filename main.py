e2bf6a1dd0495bb0650ea8848bde4479b6d7b4b7
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-09-06 19:23:22 +0530

    add basic structure to blockchain class

---
+++ main.py
@@ -0,0 +1,16 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # adds a new transaction to the list of transactions
+        pass
+    @staticmethod
+    def hash(block):
+        # hashes a block
+        pass
