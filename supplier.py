6a84b2d8e3b9a566e1a96961acc920c2a6f5aeb9
ayush-is-peace <nympseudo259@gmail.com>
2022-09-08 18:21:32 +0530

    Add timestamp as well as datetime to block

--- supplier.py
+++ supplier.py
@@ -1,6 +1,6 @@
 import hashlib
 import json
-import datetime
+from time import time, ctime
 from uuid import uuid4
 from flask import Flask, jsonify, request
 
@@ -13,23 +13,22 @@ class Supplier:
 
     def add_new_block(self, proof, previous_hash=None):
         block = {
-            'index': len(self.chain) + 1,
-            'date': datetime.datetime.now(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1])
+            "index": len(self.chain) + 1,
+            "date_time": ctime(),
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1]),
         }
         self.current_transactions = []
         self.chain.append(block)
         return block
 
     def new_transaction(self, supplier, manufacturer, raw_material):
-        self.current_transactions.append({
-            'sender': supplier,
-            'recipient': manufacturer,
-            'item': raw_material
-        })
-        return self.recent_block['index'] + 1
+        self.current_transactions.append(
+            {"sender": supplier, "recipient": manufacturer, "item": raw_material}
+        )
+        return self.recent_block["index"] + 1
 
     @property
     def recent_block(self):
@@ -48,7 +47,7 @@ class Supplier:
 
     @staticmethod
     def valid_proof(last_proof, proof):
-        guess = f'{last_proof}{proof}'.encode()
+        guess = f"{last_proof}{proof}".encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
@@ -59,49 +58,49 @@ node_identifier2 = str(uuid4())
 S = Supplier()
 
 
-@app.route('/supply', methods=['GET'])
+@app.route("/supply", methods=["GET"])
 def supply():
     recent_block = S.recent_block
-    last_proof = recent_block['proof']
+    last_proof = recent_block["proof"]
     proof = S.proof_of_work(last_proof)
     S.new_transaction(
         supplier=node_identifier1,
         manufacturer=node_identifier2,
-        raw_material='raw_material',
+        raw_material="raw_material",
     )
     previous_hash = S.hash(recent_block)
     block = S.add_new_block(proof, previous_hash)
 
     res = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
     }
     return jsonify(res), 200
 
 
-@app.route('/supply/transaction', methods=['POST'])
+@app.route("/supply/transaction", methods=["POST"])
 def new_transaction():
     values = request.get_json()
-    required = ['sender', 'recipient', 'item']
+    required = ["sender", "recipient", "item"]
     if not all(k in values for k in required):
-        return 'Missing values', 400
-    index = S.new_transaction(values['sender'], values['recipient'], values['item'])
+        return "Missing values", 400
+    index = S.new_transaction(values["sender"], values["recipient"], values["item"])
 
-    res = {'message': f'Transaction will be added to Block {index}'}
+    res = {"message": f"Transaction will be added to Block {index}"}
     return jsonify(res), 201
 
 
-@app.route('/blockchain', methods=['GET'])
+@app.route("/blockchain", methods=["GET"])
 def full_chain():
     res = {
-        'chain': S.chain,
-        'length': len(S.chain),
+        "chain": S.chain,
+        "length": len(S.chain),
     }
     return jsonify(res), 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
