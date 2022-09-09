f076d8997f967a265f50d21c2ba2ca1b74e284c3
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-09-10 01:16:25 +0530

    enforce black codestyle

--- main.py
+++ main.py
@@ -113,28 +113,29 @@ def setup_server():
     def mine():
         # We run the proof of work algorithm to get the next proof
         last_block = blockchain.last_block
-        last_proof = last_block['proof']
+        last_proof = last_block["proof"]
         proof = blockchain.proof_of_work(last_proof)
         # We must receive a reward for finding the proof.
         # The sender is "0" to signify that this node has mined a new coin.
         blockchain.new_transaction(
-            sender='0',
+            sender="0",
             recipient=node_identifier,
             amount=1,
         )
         # Forge the new block by adding it to the chain
         previous_hash = blockchain.hash(last_block)
-        block = blockchain.new_block(proof,previous_hash)
+        block = blockchain.new_block(proof, previous_hash)
         response = {
-            'message':"New block forged",
-            'index':block['index'],
-            'transactions':block['transactions'],
-            'proof':block['proof'],
-            'previous_hash':block['previous_hash'],
+            "message": "New block forged",
+            "index": block["index"],
+            "transactions": block["transactions"],
+            "proof": block["proof"],
+            "previous_hash": block["previous_hash"],
         }
-        return jsonify(response),200
+        return jsonify(response), 200
+
     @app.route("/transactions/new", methods=["POST"])
-    def new_transaction(): # a method for creating transactions
+    def new_transaction():  # a method for creating transactions
         values = request.get_json()
         # check that the required fields are in the POSTed data
         required = ["sender", "recipient", "amount"]
