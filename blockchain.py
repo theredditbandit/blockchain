2f3722dae2b66abc88812bc70d183cd024ee9005
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2022-09-10 16:10:23 +0530

    Implement the consensus algorithm

--- blockchain.py
+++ blockchain.py
@@ -109,6 +109,38 @@ class Blockchain(object):
 
             return True
 
+    def resolve_conflicts(self):
+        """
+        This is our consensus algorithm , it resolves conflicts by replacing our chain with the longest one in the network.
+        :return:<bool> True if our chain was replaced , False if not
+        """
+        neighbours = self.nodes
+        new_chain = None
+
+        # we're only looking for chains longer than ours
+        max_length = len(self.chain)
+        # Grab and verify the chains from all th nodes in our network
+        for node in neighbours:
+            response = requests.get(f"http://{node}/chain")
+
+            if response.status_code == 200:
+                length = response.json()["length"]
+                chain = response.json()["chain"]
+
+                # check if the length is longer and the chain is valid
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+            # Replace our chain if we discovered a new valid chain longer than the current one
+
+            if new_chain:
+                self.chain = new_chain
+                return True
+
+            return False
+
     @staticmethod
     def valid_proof(last_proof, proof):
         """
